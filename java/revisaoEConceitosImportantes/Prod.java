8ff98142c405ed2d21fd13bfe4b83d784b4b99b4
Augusto <augustocesarrezende64@gmail.com>
2024-12-09 12:39:24 -0300

    classe java modificada e gitignore atualizado

--- java/revisaoEConceitosImportantes/Prod.java
+++ java/revisaoEConceitosImportantes/Prod.java
@@ -1,16 +1,43 @@
 public class Prod {
-    String nome;
-    double preco;
-    int quantidade;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
     public Prod(String nome, double preco, int quantidade) {
         this.nome = nome;
         this.preco = preco;
         this.quantidade = quantidade;
     }
+
     public double getTotal() {
         return preco * quantidade;
     }
+
     public void setAtualizaPreco(double porcentagemDesconto) {
         this.preco = preco * (1 + porcentagemDesconto / 100);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
     }
 }
